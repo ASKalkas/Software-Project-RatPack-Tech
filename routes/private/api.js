d73043341c8be4fb7d3c22fb9f4febb8dd920d51
Raijin-ZD <112382292+Raijin-ZD@users.noreply.github.com>
2023-05-25 20:30:48 +0300

    update private update routes done @RaijinZD
    him self did it in less than 2 seconds

--- routes/private/api.js
+++ routes/private/api.js
@@ -72,5 +72,93 @@ module.exports = function (app) {
     }
   });
 
+  app.put("/api/v1/route/:routeId", async function (req, res) {
+    const { routeId } = req.params;// access the request parameters
+    const routeName = req.body.routename;
+    const existingRoutes = await db
+      .select("*")
+      .from("se_project.routes")
+      .where("id", routeId)
+      .first();
+  
+    if (!existingRoutes) {
+      return res.status(404).send("Routes does not exist");
+    }
+     try {
+      await db("se_project.routes")
+      .where("id", routeId)
+      .update({
+        routename : routeName
+      })
+      .returning("*");
+      // Return a success message.
+     res.status(200).send("Route updated successfully");
+
+    } 
+   catch (e) {
+    console.log(e.message);
+    res.status(400).json({error: "Unable to update Route name.", });
+   }
+  });
+    // Get the new name of the station.
+  
+  // Update the station name.
+  //const success = updateStation(stationId, newStationName);
+
+  // Send a response to the client.
+  // if (success) {
+  //   res.status(200).json({
+  //     success: true,
+  //   });
+  // } else {
+  //   res.status(400).json({
+  //     error: "Unable to update station name.",
+  //   });
+  
+    // Update the station in the database.
+    //await db
+      //.update("se_project.stations")
+    //  .where("id", stationId)
+    //  .set({ stationName });
+  
+    
+   
+/*  
+app.delete("/api/v1/station/:stationId", async function (req, res) {
+  const { stationId } = req.params;
+
+  // Check if the station exists.
+  const existingStation = await db
+    .select("*")
+    .from("se_project.stations")
+    .where("id", stationId)
+    .first();
+
+  if (!existingStation) {
+    // If the station does not exist, return an HTTP not found code.
+    return res.status(404).send("Station does not exist");
+  }
+
+  // Delete the station from the database.
+  await db
+    .delete("se_project.stations")
+    .where("id", stationId);
+
+  // Return a success message.
+  res.status(200).send("Station deleted successfully");
+});
+app.get("/users", async function (req, res) {
+  try {
+     const user = await getUser(req);
+    const users = await db.select('*').from("se_project.users")
+      
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(400).send("Could not get users");
+  }
+ 
+});
+*/
   
 };
